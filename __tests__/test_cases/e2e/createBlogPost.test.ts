77d9d67f6159113a86f9bee7d61576c0f211e6e3
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2024-04-23 09:42:39 +0800

    fix: lint and gitignore

--- __tests__/test_cases/e2e/createBlogPost.test.ts
+++ __tests__/test_cases/e2e/createBlogPost.test.ts
@@ -1,40 +1,40 @@
 require("dotenv").config({
-	path: `.env.${process.env.STAGE}`,
+  path: `.env.${process.env.STAGE}`,
 });
 import axios from "axios";
 const chance = require("chance").Chance();
 
 describe("When we call POST /blogposts", () => {
-	it("should create a new item", async () => {
-		console.log(`API_BASE_URL: ${process.env.API_BASE_URL}`);
-		const title = chance.sentence({ words: 5 });
-		const content = chance.paragraph();
+  it("should create a new item", async () => {
+    console.log(`API_BASE_URL: ${process.env.API_BASE_URL}`);
+    const title = chance.sentence({ words: 5 });
+    const content = chance.paragraph();
 
-		let response;
-		try {
-			response = await axios.post(
-				`${process.env.API_BASE_URL}/blogposts`,
-				{
-					title,
-					content,
-				},
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "test",
-					},
-				}
-			);
-		} catch (error: any) {
-			console.error(error);
-		}
+    let response;
+    try {
+      response = await axios.post(
+        `${process.env.API_BASE_URL}/blogposts`,
+        {
+          title,
+          content,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "test",
+          },
+        },
+      );
+    } catch (error: any) {
+      console.error(error);
+    }
 
-		expect(response).toBeDefined();
+    expect(response).toBeDefined();
 
-		if (response) {
-			expect(response.status).toBe(201);
-			expect(response.data.data.title).toBe(title);
-			expect(response.data.data.content).toBe(content);
-		}
-	});
+    if (response) {
+      expect(response.status).toBe(201);
+      expect(response.data.data.title).toBe(title);
+      expect(response.data.data.content).toBe(content);
+    }
+  });
 });
--- __tests__/test_cases/integration/createBlogPost.test.ts
+++ __tests__/test_cases/integration/createBlogPost.test.ts
@@ -1,29 +1,29 @@
 require("dotenv").config({
-	path: `.env.${process.env.STAGE}`,
+  path: `.env.${process.env.STAGE}`,
 });
 const chance = require("chance").Chance();
 
 describe("createBlogPost", () => {
-	it("should create a blog post", async () => {
-		const handler = require("@src/functions/createBlogPost").handler;
+  it("should create a blog post", async () => {
+    const handler = require("@src/functions/createBlogPost").handler;
 
-		const title = chance.sentence();
-		const content = chance.paragraph();
+    const title = chance.sentence();
+    const content = chance.paragraph();
 
-		const event = {
-			body: JSON.stringify({
-				title,
-				content,
-			}),
-		};
+    const event = {
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+    };
 
-		const response = await handler(event);
+    const response = await handler(event);
 
-		expect(response.statusCode).toEqual(201);
-		expect(response.body).toBeDefined();
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toBeDefined();
 
-		const parsedResponse = JSON.parse(response.body);
-		expect(parsedResponse.data.title).toBe(title);
-		expect(parsedResponse.data.content).toBe(content);
-	});
+    const parsedResponse = JSON.parse(response.body);
+    expect(parsedResponse.data.title).toBe(title);
+    expect(parsedResponse.data.content).toBe(content);
+  });
 });
